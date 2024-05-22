97b70006ba3259b2875a94f9b6e3733a1a31db75
YooInsik <yuins@i-ops.co.kr>
2024-05-22 14:44:31 +0900

    [FASTCAMPUS] 상품이미지,옵션정보 가져오기/ 에러발생 처리 / Context를 사용해서 데이터 제공 / State 업데이트 함수 추가 / 상품 Count를 이용한 가격 계산

--- client/src/components/Type.js
+++ client/src/components/Type.js
@@ -1,12 +1,57 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import Products from "./Products";
+import Options from "./Options";
+import ErrorBanner from "./ErrorBanner";
+import { OrderContext } from "../context/OrderContext";
 
-const Type = ({orderType}) => {
+const Type = ({ orderType }) => {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+  const [orderData, updateItemCount] = useContext(OrderContext);
+
+  useEffect(() => {
+    loadItems(orderType);
+    console.log(orderData);
+    console.log(updateItemCount);
+  }, [orderType]);
+
+  const loadItems = async (orderType) => {
+    try {
+      const response = await axios.get(`http://localhost:4000/${orderType}`);
+      setItems(response.data);
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  const ItemComponent = orderType === "products" ? Products : Options;
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    ></ItemComponent>
+  ));
+  if (error) {
+    return <ErrorBanner message="에러가 발생했습니다."></ErrorBanner>;
+  }
+
   return (
-    <div><h2>{orderType}</h2>
-    <p>하나의 가격</p>
-    <p>총 가격:</p>
-    <div style={{display:'flex', flexDirection: orderType ==="options" ? "column":"row"}}></div></div>
-  )
-}
+    <div>
+      <h2>{orderType}</h2>
+      <p>하나의 가격</p>
+      <p>총 가격:</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: orderType === "options" ? "column" : "row",
+        }}
+      >
+        {optionItems}
+      </div>
+    </div>
+  );
+};
 
-export default Type
+export default Type;
--- client/src/pages/OrderPage/index.js
+++ client/src/pages/OrderPage/index.js
@@ -6,14 +6,15 @@ const OrderPage = () => {
     <div>
       <h1>Travel Products</h1>
       <div>
-        <Type orderType="products"/>
+        <Type orderType="products" />
       </div>
-      <div style={{ display:"flex",marginTop:20}}>
-        <div style={{ width:"50%"}}>
-          <Type orderType="options"/>
+      <div style={{ display: "flex", marginTop: 20 }}>
+        <div style={{ width: "50%" }}>
+          <Type orderType="options" />
         </div>
-        <div style={{ width:"50%"}}>
-          <h2>Total Price: </h2><br/>
+        <div style={{ width: "50%" }}>
+          <h2>Total Price: </h2>
+          <br />
           <button>주문</button>
         </div>
       </div>
